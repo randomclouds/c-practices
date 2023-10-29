95b178d6326e4b7357eb7ad43c946302036e7164
randomclouds <3385398842@qq.com>
2023-10-29 20:19:59 +0800

    2023.10.29 modify2

--- file/file/sort.c
+++ file/file/sort.c
@@ -2,35 +2,35 @@
 #pragma warning(disable:6031)
 #include <stdio.h>
 
-//void sort(int arr[10]) {
-//	for (int i = 0; i < 10; i++) {
-//		int flag = 1;
-//		for (int j = 0; j < 10 - i - 1; j++) {
-//			if (arr[j] > arr[j + 1]) {
-//				int tem = arr[j];
-//				arr[j] = arr[j + 1];
-//				arr[j + 1] = tem;
-//				flag = 0;
-//			}
-//		}
-//		if (flag == 1)
-//			break;
-//	}
-//}
-//
-//int main() {
-//	int arr[10] = { 0 };
-//	for (int i = 0; i < 10; i++) {
-//		scanf("%d", &arr[i]);
-//	}
-//	for (int i = 0; i < 10; i++) {
-//		printf("%d ", arr[i]);
-//	}
-//	printf("\n");
-//	sort(arr);
-//	for (int i = 0; i < 10; i++) {
-//		printf("%d ", arr[i]);
-//	}
-//	printf("\n");
-//	return 0;
-//}
+void sort(int arr[10]) {
+	for (int i = 0; i < 10; i++) {
+		int flag = 1;
+		for (int j = 0; j < 10 - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int tem = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tem;
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+}
+
+int main() {
+	int arr[10] = { 0 };
+	for (int i = 0; i < 10; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	sort(arr);
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
