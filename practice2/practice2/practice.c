f195505b791d62ff4ce85884efe91d3eba07a5d8
randomclouds <3385398842@qq.com>
2023-11-01 12:37:47 +0800

    2023/11/1  practice2023/11/1  practice2023/11/1  practice2023/11/1 practice2023/11/1  practice2023/11/1  practice2023/11/1 practice2023/11/1  practice2023/11/1  practice2023/11/1 practice2023/11/1  practice

--- practice2/practice2/practice.c
+++ practice2/practice2/practice.c
@@ -101,3 +101,149 @@
 //    p = NULL;
 //    return 0;
 //}
+
+
+
+//int main() {
+//    int begin, arrival;
+//    scanf("%d %d", &begin, &arrival);
+//    int h = arrival / 100 + begin / 100;
+//    int m = arrival % 100 - begin % 100;
+//    int total = h * 60 + m;
+//    h = total / 60;
+//    m = total % 60;
+//    if (h < 10) {
+//        if (m < 10) printf("0%d:0%d", h, m);
+//        else printf("0%d:%d", h, m);
+//    }
+//    else {
+//        if (m < 10) printf("%d:0%d", h, m);
+//        else printf("%d:%d", h, m);
+//    }
+//    return 0;
+//}
+
+
+//int main() {
+//    int speed, limit;
+//    scanf("%d %d", &speed, &limit);
+//    int d = (double)speed / limit * 100 + 0.5;
+//    if (d < 110)printf("OK\n");
+//    else if (d < 150) printf("Exceed %d%%. Ticket 200\n", d - 100);
+//    else printf("Exceed %d%%, License Revoked", d - 100);
+//    return 0;
+//}
+
+//int main() {
+//    int a = 0;
+//    scanf("%d", &a);
+//    switch (a / 10){
+//    case 10:
+//    case 9:
+//        printf("A\n");
+//        break;
+//    case 8:
+//        printf("B\n");
+//        break;
+//    case 7:
+//        printf("C\n");
+//        break;
+//    case 6:
+//        printf("D\n");
+//        break;
+//    default:
+//        printf("E\n");
+//    }
+//    return 0;
+//}
+//
+//int main() {
+//    int a = 0, b = 0;
+//    scanf("%d %d", &a, &b);
+//
+//    int n, m, k;
+//    scanf("%d %d %d", &n, &m, &k);
+//    int bGain = m + n + k;
+//    if (a < b) {
+//        if (bGain) printf("The winner is b: %d + %d", b, bGain);
+//        else printf("The winner is a: %d + 3", a);
+//    }
+//    if (a > b) {
+//        if (bGain == 3) printf("The winner is b: %d + 3", b);
+//        else printf("The winner is a: %d + %d", a, 3 - bGain);
+//    }
+//    return 0;
+//}
+
+//ºóÐø±éÀú
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    if (newNode == NULL) {
+        return 1;
+    }
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+    if (root == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    struct TreeNode** stack = (struct TreeNode**)malloc(sizeof(struct TreeNode*) * 1000);
+    int top = -1;
+    struct TreeNode* curr = root;
+    struct TreeNode* prev = NULL;
+    int* result = (int*)malloc(sizeof(int) * 1000);
+    int index = 0;
+
+    while (top != -1 || curr != NULL) {
+        if (curr != NULL) {
+            stack[++top] = curr;
+            curr = curr->left;
+        }
+        else {
+            struct TreeNode* node = stack[top];
+            if (node->right == NULL || node->right == prev) {
+                result[index++] = node->val;
+                prev = node;
+                top--;
+            }
+            else {
+                curr = node->right;
+            }
+        }
+    }
+
+    *returnSize = index;
+    return result;
+}
+
+int main() {
+    struct TreeNode* root = createNode(1);
+    root->right = createNode(2);
+    root->right->left = createNode(3);
+
+    int returnSize;
+    int* result = postorderTraversal(root, &returnSize);
+
+    printf("Postorder Traversal: ");
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+
+    free(result);
+
+    return 0;
+}
