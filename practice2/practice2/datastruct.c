fe8b7e68b7f72e12efa8052b7cc092d438203023
randomclouds <3385398842@qq.com>
2023-11-07 23:06:08 +0800

    2023/11/07  practice

---
+++ practice2/practice2/datastruct.c
@@ -0,0 +1,368 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#define MAX 100
+#include <stdlib.h>
+#include <stdio.h>
+////List realised by array
+//
+//typedef struct LNode{
+//	int arr[MAX];
+//	int Last;
+//}*List;
+////初始化
+//List MakeEmpty() {
+//	List L;
+//
+//	L = (List)malloc(sizeof(struct LNode));
+//	L->Last = -1;
+//
+//	return L;
+//}
+//
+////查找
+//#define ERROR -1
+//
+//int Find(List L, int x) {
+//	int i = 0;
+//	while (i <= L->Last && L->arr[i] != x)
+//		i++;
+//	if (i > L->Last) return ERROR;
+//	else return i;
+//}
+//
+////插入
+//int Insert(List L, int x, int pos) {
+//	int i;
+//	if (L->Last == MAX - 1) {
+//		//表满
+//		printf("已满\n");
+//		return 1;
+//	}
+//	if (pos < 0 || pos > L->Last + 1) {
+//		printf("位置非法\n");
+//		return 1;
+//	}
+//	for (i = L->Last ; i >= pos; i--)
+//		L->arr[i + 1] = L->arr[i];
+//	L->arr[pos] = x;
+//	L->Last++;
+//	return 0;
+//}
+//
+////删除
+//int Delete(List L, int pos) {
+//	int i;
+//	if (pos < 0 || pos > L->Last) {
+//		printf("位置%d不存在\n", pos);
+//		return 1;
+//	}
+//	for (i = pos; i < L->Last; i++)
+//		L->arr[pos] = L->arr[pos + 1];
+//	L->Last--;
+//	return 0;
+//}
+//
+//int main() {
+//	List L = MakeEmpty();
+//	Insert(L, 1, 0);
+//	Insert(L, 2, 1);
+//	Insert(L, 3, 2);
+//	Insert(L, 4, 2);
+//	int pos, i = 1;
+//	while (Find(L, i) != ERROR) {
+//		pos = Find(L, i);
+//		printf("%d\n", pos);
+//		i++;
+//	}
+//	return 0;
+//}
+
+////链表实现
+//
+//typedef struct LNode {
+//	int data;
+//	struct LNode* next;
+//}*List;
+//
+////查找
+//#define ERROR 
+//List Find(List L, int x) {
+//	List cur = L;
+//	while (cur && cur->data != x)
+//		cur = cur->next;
+//	if (cur)
+//		return cur;
+//	else
+//		return ERROR;
+//}
+//
+////插入
+//int Insert(List L, int x, List pos) {
+//	List tmp, pre;
+//	for (pre = L; pre && pre->next != pos; pre = pre->next) {
+//		if (pre == NULL) {
+//			printf("错误\n");
+//			return 1;
+//		}
+//		else {
+//			tmp = (List)malloc(sizeof(struct LNode));
+//			if (!tmp) {
+//				printf("这里一般没有错\n");
+//				return 1;
+//			}
+//			tmp->data = x;
+//			tmp->next = pos;
+//			pre->next = tmp;
+//			return 0;
+//		}
+//	}
+//}
+//
+////删除
+//int Delete(List L, List pos) {
+//	List pre;
+//	for (pre = L; pre && pre->next != pos; pre = pre->next) {
+//		if (pre == NULL || pos == NULL) {
+//			printf("位置错误\n");
+//			return 1;
+//		}
+//		else {
+//			pre->next = pos->next;
+//			free(pos);
+//			return 0;
+//		}
+//	}
+//}
+//
+//int main() {
+//	return 0;
+//}
+
+
+//堆栈
+//顺序
+//typedef struct SNode {
+//	int* data;
+//	int top;
+//	int Maxsize;
+//}*Stack;
+//
+//Stack CreateStack(int Maxsize) {
+//	Stack S = (Stack)malloc(sizeof(struct SNode));
+//	S->data = (int*)malloc(Maxsize * sizeof(int));
+//	S->top = -1;
+//	S->Maxsize = Maxsize;
+//  return S;
+//}
+//
+//int IsFull(Stack S) {
+//	return (S->top == S->Maxsize);
+//}
+//
+//int Push(Stack S, int x) {
+//	if (IsFull(S)) {
+//		printf("堆满了\n");
+//		return 0;
+//	}
+//	else {
+//		S->data[++(S->top)] = x;
+//		return 1;
+//	}
+//}
+//
+//int IsEmpty(Stack S) {
+//	return (S->top == -1);
+//}
+//
+//int Pop(Stack S) {
+//	if (IsEmpty(S)) {
+//		printf("堆栈空\n");
+//		return 0;
+//	}
+//	else {
+//		return S->data[(S->top)--];
+//	}
+//}
+//
+//int main() {
+//	Stack S = CreateStack(10);
+//	for (int i = 0; i < 10; i++) {
+//		Push(S, i);
+//	}
+//	for (int i = 0; i < 10; i++) {
+//		printf("%d ", Pop(S));
+//	}
+//	return 0;
+//}
+
+////链表存储
+//typedef struct SNode {
+//	int data;
+//	struct SNode* next;
+//}* Stack;
+//
+//Stack CreateStack() {
+//	Stack S;
+//
+//	S = (Stack)malloc(sizeof(struct SNode));
+//	S->next = NULL;
+//	return S;
+//}
+//
+//int IsEmpty(Stack S) {
+//	return (S->next == NULL);
+//}
+//
+//int Push(Stack S, int x) {
+//	Stack tmp;
+//	tmp = (Stack)malloc(sizeof(struct SNode));
+//	tmp->data = x;
+//	tmp->next = S->next;
+//	S->next = tmp;
+//	return 1;
+//}
+//
+//int Pop(Stack S) {
+//	Stack tmp;
+//	int top;
+//	if (IsEmpty) {
+//		printf("堆空\n");
+//		return 0;
+//	}
+//
+//	else {
+//		tmp = S->next;
+//		top = tmp->data;
+//		S->next = tmp->next;
+//		free(tmp);
+//		return top;
+//	}
+//}
+//
+//int main() {
+//	return 0;
+//}
+
+//队列
+//数组
+//
+//typedef struct QNode {
+//	int* data;
+//	int Front, Rear;
+//	int MaxSize;
+//}* Queue;
+//
+//Queue CreateQueue(int MaxSize) {
+//	Queue Q = (Queue)malloc(sizeof(struct QNode));
+//	Q->data = (int*)malloc(MaxSize * sizeof(int));
+//	Q->Front = Q->Rear = 0;
+//	Q->MaxSize = MaxSize;
+//	return Q;
+//}
+//
+//int IsFull(Queue Q) {
+//	return (Q->Rear + 1) % Q->MaxSize == Q->Front;
+//}
+//
+//int AddQ(Queue Q, int x) {
+//	if (IsFull(Q)) {
+//		printf("队列满\n");
+//		return 0;
+//	}
+//
+//	else {
+//		Q->Rear = (Q->Rear + 1) % Q->MaxSize;
+//		Q->data[Q->Rear] = x;
+//		return 1;
+//	}
+//}
+//
+//int IsEmpty(Queue Q) {
+//	return (Q->Front == Q->Rear);
+//}
+//
+//int DeleteQ(Queue Q) {
+//	if (IsEmpty(Q)) {
+//		printf("队列空\n");
+//		return 0;
+//	}
+//
+//	else {
+//		Q->Front = (Q->Front + 1) % Q->MaxSize;
+//		return Q->data[Q->Front];
+//	}
+//}
+//
+//int main() {
+//	Queue Q = CreateQueue(10);
+//	for (int i = 0; i < 9; i++) {
+//		AddQ(Q, i);
+//	}
+//	for (int i = 0; i < 9; i++) {
+//		printf("%d ",DeleteQ(Q));
+//	}
+//	return 0;
+//}
+
+//typedef struct Node {
+//	int Data;
+//	struct Node* Next;
+//}* PtrToNode;
+//
+//typedef struct QNode {
+//	PtrToNode Front, Rear;
+//	int Maxsize;
+//}* Queue;
+//
+//int IsEmpty(Queue Q) {
+//	return (Q->Front == NULL);
+//}
+//
+//int Delete(Queue Q) {
+//	PtrToNode FrontCell;
+//	int FrontElem;
+//
+//	if (IsEmpty(Q)) {
+//		printf("Queue is empty\n");
+//		return 0;
+//	}
+//
+//	else {
+//		FrontCell = Q->Front;
+//		if (Q->Front == Q->Rear)
+//			Q->Front = Q->Rear = NULL;
+//		else
+//			Q->Front = Q->Front->Next;
+//		FrontElem = FrontCell->Data;
+//		free(FrontCell);
+//		return FrontElem;
+//	}
+//}
+
+//typedef struct TNode {
+//	int Data;
+//	struct TNode Left, Right;
+//}* BinTree;
+
+//int main() {
+//	printf("2\n");
+//	return 0;
+//}
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    double* arr = (double*)malloc(sizeof(double)*a);
+    char ch;
+    for (int i = 0; i < a; i++) {
+        getchar();
+        scanf("%c %lf", &ch, &arr[i]);
+        if (ch == 'M') arr[i] /= 1.09;
+        else  arr[i] *= 1.09;
+    }
+    for (int i = 0; i < a; i++) {
+        printf("%.2f\n", arr[i]);
+    }
+    free(arr);
+    return 0;
+}
--- practice2/practice2/practice.c
+++ practice2/practice2/practice.c
@@ -2,7 +2,7 @@
 #pragma warning(disable:6031)
 #include<stdio.h>
 #include<stdlib.h>
-
+#include <math.h>
 //typedef struct ski { 
 //	int value;
 //	int hur;
@@ -177,73 +177,371 @@
 
 //ºóÐø±éÀú
 
-struct TreeNode {
-    int val;
-    struct TreeNode* left;
-    struct TreeNode* right;
-};
+//struct TreeNode {
+//    int val;
+//    struct TreeNode* left;
+//    struct TreeNode* right;
+//};
+//
+//struct TreeNode* createNode(int val) {
+//    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+//    if (newNode == NULL) {
+//        return 1;
+//    }
+//    newNode->val = val;
+//    newNode->left = NULL;
+//    newNode->right = NULL;
+//    return newNode;
+//}
+//
+//int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+//    if (root == NULL) {
+//        *returnSize = 0;
+//        return NULL;
+//    }
+//
+//    struct TreeNode** stack = (struct TreeNode**)malloc(sizeof(struct TreeNode*) * 1000);
+//    int top = -1;
+//    struct TreeNode* curr = root;
+//    struct TreeNode* prev = NULL;
+//    int* result = (int*)malloc(sizeof(int) * 1000);
+//    int index = 0;
+//
+//    while (top != -1 || curr != NULL) {
+//        if (curr != NULL) {
+//            stack[++top] = curr;
+//            curr = curr->left;
+//        }
+//        else {
+//            struct TreeNode* node = stack[top];
+//            if (node->right == NULL || node->right == prev) {
+//                result[index++] = node->val;
+//                prev = node;
+//                top--;
+//            }
+//            else {
+//                curr = node->right;
+//            }
+//        }
+//    }
+//
+//    *returnSize = index;
+//    return result;
+//}
+//
+//int main() {
+//    struct TreeNode* root = createNode(1);
+//    root->right = createNode(2);
+//    root->right->left = createNode(3);
+//
+//    int returnSize;
+//    int* result = postorderTraversal(root, &returnSize);
+//
+//    printf("Postorder Traversal: ");
+//    for (int i = 0; i < returnSize; i++) {
+//        printf("%d ", result[i]);
+//    }
+//    printf("\n");
+//
+//    free(result);
+//
+//    return 0;
+//}
 
-struct TreeNode* createNode(int val) {
-    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-    if (newNode == NULL) {
-        return 1;
-    }
-    newNode->val = val;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
-}
 
-int* postorderTraversal(struct TreeNode* root, int* returnSize) {
-    if (root == NULL) {
-        *returnSize = 0;
-        return NULL;
-    }
+//int main() {
+//	int N;
+//	scanf("%d", &N);
+//	for (int i = 0; i <= N; i++) {
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+//void print(int N) {
+//	if (N) {
+//		print(N - 1);
+//		printf("%d\n", N);
+//	}
+//	return 0;
+//}
+//
+//int main() {
+//	int N;
+//	scanf("%d", &N);
+//	print(N);
+//	return 0;
+//}
+//
+//#include<time.h>
+//#include<math.h>
+//#define MAXU 1e5
+//clock_t star, stop;
+//
+//double cal(double a[], int x, int max) {
+//	double sum = 0;
+//	for (int i = 0; i < max; i++) {
+//		sum += a[i] * pow(x,i);
+//	}
+//	return sum;
+//}
+//
+//double cal2(double a[], int x, int max) {
+//	double sum = 0;
+//	for (int i = 0; i < max; i++) {
+//		sum = sum * x + a[max - i - 1] / i;
+//	}
+//	return 0;
+//}
+//
+//void use(double(*fp)(double[], int, int), double a[], int x, int max) {
+//	star = clock();
+//	for (int i = 0; i < MAXU; i++)
+//		fp(a, x, max);
+//	stop = clock();
+//	double durationl = ((double)(stop - star)) / CLK_TCK;
+//	printf("Ticket = %f\n", (double)(stop - star));
+//	printf("durationl = %6.2e\n", durationl);
+//}
+//
+//int main() {
+//	double arr[100] = { 0 }, x = 0;
+//	arr[0] = 1.0;
+//	for (int i = 1; i < 100; i++) {
+//		arr[i] = 1 / i;
+//	}
+//	scanf("%lf", &x);
+//	use(&cal, arr, x, 100);
+//	use(&cal2, arr, x, 10);
+//	return 0;
+//}
 
-    struct TreeNode** stack = (struct TreeNode**)malloc(sizeof(struct TreeNode*) * 1000);
-    int top = -1;
-    struct TreeNode* curr = root;
-    struct TreeNode* prev = NULL;
-    int* result = (int*)malloc(sizeof(int) * 1000);
-    int index = 0;
+//int main() {
+//    int salary;
+//    scanf("%d", &salary);
+//
+//    double sum = 0;
+//    if (salary <= 1600) printf("%.2f\n", sum);
+//    else if (salary <= 2500) {
+//        sum = (salary - 1600) * 0.05;
+//        printf("%.2f\n", sum);
+//    }
+//    else if (salary <= 3500) {
+//        sum = (salary - 1600) * 0.10;
+//        printf("%.2f\n", sum);
+//    }
+//    else if (salary <= 4500) {
+//        sum += (salary - 1600) * 0.15;
+//        printf("%.2f\n", sum);
+//    }
+//    else if (salary > 4500) {
+//        sum += (salary - 1600) * 0.2;
+//        printf("%.2f\n", sum);
+//    }
+//    return 0;
+//}
 
-    while (top != -1 || curr != NULL) {
-        if (curr != NULL) {
-            stack[++top] = curr;
-            curr = curr->left;
-        }
-        else {
-            struct TreeNode* node = stack[top];
-            if (node->right == NULL || node->right == prev) {
-                result[index++] = node->val;
-                prev = node;
-                top--;
-            }
-            else {
-                curr = node->right;
-            }
-        }
-    }
+//int main() {
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//    if (a + b > c && a + c > b && b + c > a) {
+//        double per = a + b + c;
+//        double s = per / 2;
+//        double area = sqrt(s * (s - a) * (s - b) * (s - c));
+//        printf("area = %.2f; perimeter = %.2f\n", area, per);
+//    }
+//    else printf("These sides do not correspond to a valid triangle\n");
+//    return 0;
+//}
 
-    *returnSize = index;
-    return result;
-}
+//int main() {
+//    double a, b, c;
+//    scanf("%lf %lf %lf", &a, &b, &c);
+//    double de = b * b - 4 * a * c;
+//    if (a == 0 && b == 0 && c == 0) {
+//        printf("Zero Equation\n");
+//        return 0;
+//    }
+//    if (a == 0 && b == 0 && c) {
+//        printf("Not An Equation\n");
+//        return 0;
+//    }
+//    if (a == 0) {
+//        if (c == 0) printf("0.00\n");
+//        else printf("%.2f", -c / b);
+//    }
+//    else {
+//        if (de < 0) {
+//            de = sqrt(-de);
+//            if (b) {
+//                printf("%.2f+%.2fi\n", -b / (2 * a), de / (2 * a));
+//                printf("%.2f-%.2fi\n", -b / (2 * a), de / (2 * a));
+//            }
+//            else {
+//                printf("0.00+%.2fi\n", de / (2 * a));
+//                printf("0.00-%.2fi\n", de / (2 * a));
+//            }
+//        }
+//        else if (de > 0) {
+//            de = sqrt(de);
+//            double x1, x2;
+//            x1 = -b / (2 * a) + de / (2 * a);
+//            x2 = -b / (2 * a) - de / (2 * a);
+//            if (x1 == 0) x1 = 0;
+//            if (x2 == 0) x2 = 0;
+//            printf("%.2f\n", x1);
+//            printf("%.2f\n", x2);
+//        }
+//        else printf("%.2f\n", -b / (2 * a));
+//    }
+//    return 0;
+//}
 
-int main() {
-    struct TreeNode* root = createNode(1);
-    root->right = createNode(2);
-    root->right->left = createNode(3);
+//int main() {
+//    int year, mon, day;
+//    scanf("%d/%d/%d", &year, &mon, &day);
+//    int arr[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//    int sum = day;
+//    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+//        for (int i = 0; i < mon - 1; i++) {
+//            sum += arr[i];
+//        }
+//        printf("%d", sum);
+//    }
+//    else {
+//        arr[1] = 28;
+//        for (int i = 0; i < mon - 1; i++) sum += arr[i];
+//        printf("%d\n", sum);
+//    }
+//
+//    return 0;
+//}
 
-    int returnSize;
-    int* result = postorderTraversal(root, &returnSize);
+//int main() {
+//    int n = 0, sum = 0;
+//    scanf("%d", &n);
+//    for (int i = 1; i <= n; i++) {
+//        int sum2 = 1;
+//        for (int j = 1; j <= i; j++) sum2 *= j;
+//        sum += sum2;
+//    }
+//    printf("%d", sum);
+//    return 0;
+//}
 
-    printf("Postorder Traversal: ");
-    for (int i = 0; i < returnSize; i++) {
-        printf("%d ", result[i]);
-    }
-    printf("\n");
+//int main() {
+//    double eps;
+//    scanf("%lf", &eps);
+//
+//    double sum = 0.0;
+//    double term = 1.0;
+//    int sign = 1;
+//    int n = 1;
+//
+//    while (fabs(term) > eps) {
+//        sum += term;
+//        sign = -sign;
+//        n += 3;
+//        term = sign / (double)n;
+//    }
+//    sum += term;
+//
+//    printf("sum = %.6f\n", sum);
+//
+//    return 0;
+//}
 
-    free(result);
+//int main() {
+//    int st, be;
+//    scanf("%d %d", &st, &be);
+//    int count = 0, sum = 0;
+//    for (int i = st; i <= be; i++) {
+//        int j = 2;
+//        for (; j < i; j++) {
+//            if (i % j == 0) break;
+//        }
+//        if (j == i) {
+//            count++;
+//            sum += i;
+//        }
+//    }
+//    printf("%d %d", count, sum);
+//    return 0;
+//}
 
-    return 0;
-}
+
+//int main() {
+//    int a, n, sum = 0;
+//    scanf("%d %d", &a, &n);
+//    for (int i = 0; i < n; i++) {
+//        int sum2 = a;
+//        for (int j = 0; j < i; j++) {
+//            sum2 = sum2 * 10 + a;
+//        }
+//        sum += sum2;
+//    }
+//    printf("s = %d", sum);
+//    return 0;
+//}
+
+//int main() {
+//    int M, N, a;
+//    scanf("%d %d", &M, &N);
+//    for (a = M; a % N != 0; a += M);
+//    while (M % N) {
+//        int c = M % N;
+//        M = N;
+//        N = c;
+//    }
+//    printf("%d %d\n", N, a);
+//    return 0;
+//}
+
+//int main() {
+//    double pi = 0, tmp = 1, esp;
+//    int a = 1;
+//    scanf("%lf", &esp);
+//    while (tmp > esp) {
+//        double mo = 1, son = 1;
+//        for (int i = 1; i <= a; i++) {
+//            mo *= 2.0 * i - 1.0;
+//        }
+//        for (int i = 1; i < a; i++) {
+//            son *= (double)i;
+//        }
+//        a++;
+//        tmp = son / mo;
+//        pi += tmp;
+//    }
+//    printf("%.6f\n", pi * 2);
+//    return 0;
+//}
+
+//int main() {
+//    int n, up, down;
+//    scanf("%d %d %d", &n, &up, &down);
+//    int sum = up, min = 1;
+//    while (sum < n) {
+//        sum += up - down;
+//        min += 2;
+//    }
+//    printf("%d", min);
+//    return 0;
+//}
+
+//int main() {
+//    int a;
+//    scanf("%d", &a);
+//    int* arr = (int*)malloc(a);
+//    for (int i = 0; i < a; i++) {
+//        scanf("%d", &arr[i]);
+//    }
+//
+//    int b = 0, c = 0;
+//    for (int i = 0; i < a; i++) {
+//        b += arr[i] % 2;
+//    }
+//    free(arr);
+//    c = a - b;
+//    printf("%d %d", b, c);
+//    return 0;
+//}
